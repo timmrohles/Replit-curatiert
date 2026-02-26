0e3c7815ef8eedbedc66b5af4d567cd1b6dcff48
timmrohles <timmrohles@gmail.com>
2026-02-26 09:43:41 +0100

    Full project sync from Replit

--- client/src/utils/performance/performanceTest.ts
+++
@@ -1,1 +0,0 @@
-export { runAllTests } from '../performanceTest';
--- server/replit_integrations/auth/index.ts
+++
@@ -1,3 +0,0 @@
-export { setupAuth, isAuthenticated, getSession, providerConfig } from "./replitAuth";
-export { authStorage, type IAuthStorage } from "./storage";
-export { registerAuthRoutes, requireRole } from "./routes";
